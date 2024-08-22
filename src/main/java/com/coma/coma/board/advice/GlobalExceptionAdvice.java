9b976defd8f4f3e5e40936e81670b5a5886bebad
[CLOUD4] 김준수 <dbslxl11@gmail.com>
2024-08-22 16:42:34 +0900

    feat: 게시판(board) 전역 예외처리, 에러 객체 추가

---
+++ src/main/java/com/coma/coma/board/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,34 @@
+package com.coma.coma.board.advice;
+
+import com.coma.coma.board.exception.ErrorResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+// 지시사항을 참고하여 코드를 작성해 보세요.
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    // 지시사항을 참고하여 코드를 작성해 보세요.
+    @ExceptionHandler
+    public ResponseEntity exceptionHandle(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+
+        List<ErrorResult.FieldError> errorLogs
+                = fieldErrors.stream()
+                .map(error -> new ErrorResult.FieldError(
+                        error.getField(),
+                        error.getRejectedValue(),
+                        error.getDefaultMessage()
+                ))
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(errorLogs, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/com/coma/coma/board/exception/ErrorResult.java
@@ -0,0 +1,21 @@
+package com.coma.coma.board.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResult {
+
+    private List<FieldError> fieldErrors;
+
+    @Getter
+    @AllArgsConstructor
+    public static class FieldError {
+        private String fieldName;
+        private Object rejectedValue;
+        private String message;
+    }
+}
