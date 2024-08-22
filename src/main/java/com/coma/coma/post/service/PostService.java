1f5ddcd55d1dfdc2427d5be8504b1d551bd8e003
[CLOUD4] 윤지현 <jihyeon.yun619@gmail.com>
2024-08-22 15:37:31 +0900

    feat: PostService DTO 사용하도록 변경 및 Update/Create 분리

--- src/main/java/com/coma/coma/post/service/PostService.java
+++ src/main/java/com/coma/coma/post/service/PostService.java
@@ -1,42 +1,67 @@
 package com.coma.coma.post.service;
 
+import com.coma.coma.post.dto.PostRequestDto;
+import com.coma.coma.post.dto.PostResponseDto;
 import com.coma.coma.post.entity.Post;
+import com.coma.coma.post.mapper.PostMapper;
 import com.coma.coma.post.repository.PostRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
 
     private final PostRepository postRepository;
+    private final PostMapper postMapper;
 
     // DI
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, PostMapper postMapper) {
         this.postRepository = postRepository;
+        this.postMapper = postMapper;
     }
 
-    // create / update 둘 다 여기서 처리
-    public Post createPost(Post post) {
-        return postRepository.save(post);
+    // create
+    public PostResponseDto createPost(Post post) {
+        Post saved = postRepository.save(post);
+        PostResponseDto postResponseDto = postMapper.toResponseDto(post);
+        return postResponseDto;
+    }
+
+    // update
+    public PostResponseDto updatePost(Integer postId, PostRequestDto postRequestDto) {
+        Post targetPost = postRepository.findById(postId).orElseThrow(NoSuchElementException::new);
+        targetPost.setTitle(postRequestDto.getTitle());
+        targetPost.setContent(postRequestDto.getContent());
+        postRepository.save(targetPost);
+
+        return postMapper.toResponseDto(targetPost);
     }
 
     // postId로 Post 가져오기
-    public Post findPost(int postId) {
-        return postRepository.findById(postId)
+    public PostResponseDto findPost(Integer postId) {
+        Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new NoSuchElementException());
+        PostResponseDto postResponseDto = postMapper.toResponseDto(post);
+        return postResponseDto;
     }
 
     // 특정 게시판의 모든 포스트 가져오기
-    public List<Post> findAll(Long boardId) {
-        return postRepository.findAll(boardId);
+    public List<PostResponseDto> findAll(Long boardId) {
+
+        List<Post> posts = postRepository.findAll(boardId);
+        List<PostResponseDto> postResponseDtos = posts.stream()
+                .map(post -> postMapper.toResponseDto(post))
+                .collect(Collectors.toList());
+        return postResponseDtos;
     }
 
     // Id 이용해서 Post 삭제
-    public void deletePost(int postId) {
+    public void deletePost(Integer postId) {
         Post post = postRepository.findById(postId).orElseThrow(() -> new NoSuchElementException());
-
         postRepository.delete(post);
     }
 }
