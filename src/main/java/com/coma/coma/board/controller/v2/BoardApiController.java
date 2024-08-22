ae5389dbccce8069ff026e32b94b3eac6730a008
[CLOUD4] 김준수 <dbslxl11@gmail.com>
2024-08-22 14:25:00 +0900

    feat: 게시판(board) 수정, 삭제 axios로 변경

--- src/main/java/com/coma/coma/board/controller/v2/BoardApiController.java
+++ src/main/java/com/coma/coma/board/controller/v2/BoardApiController.java
@@ -2,6 +2,7 @@ package com.coma.coma.board.controller.v2;
 
 
 import com.coma.coma.board.dto.BoardCreateRequest;
+import com.coma.coma.board.dto.BoardUpdateRequest;
 import com.coma.coma.board.entity.Board;
 import com.coma.coma.board.service.BoardService;
 import org.springframework.http.HttpStatus;
@@ -19,10 +20,26 @@ public class BoardApiController {
         this.boardService = boardService;
     }
 
+
+    // 추가 api
     @PostMapping()
     public ResponseEntity<Void> createBoard(@RequestBody @Validated BoardCreateRequest boardCreateRequest) {
         Board board = boardCreateRequest.toEntity();
         boardService.createBoard(board);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 수정 api
+    @PutMapping("/{boardId}")
+    public ResponseEntity<Void> updateContact(@PathVariable("boardId") Long boardId, @RequestBody @Validated BoardUpdateRequest boardUpdateRequest) {
+        boardService.updateBoard(boardId, boardUpdateRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 삭제 api
+    @DeleteMapping("/{boardId}")
+    public ResponseEntity<Void> deleteBoard(@PathVariable("boardId") long boardId) {
+        boardService.deleteBoard(boardId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/coma/coma/board/controller/v2/BoardControllerV2.java
+++ src/main/java/com/coma/coma/board/controller/v2/BoardControllerV2.java
@@ -1,15 +1,35 @@
 package com.coma.coma.board.controller.v2;
 
+import com.coma.coma.board.entity.Board;
+import com.coma.coma.board.service.BoardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/v2/boards")
 public class BoardControllerV2 {
 
+    private final BoardService boardService;
+
+    public BoardControllerV2(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    // 추가 화면으로 이동
     @GetMapping("/create")
     public String createForm() {
         return "board/v2/createBoard";
+    }
+
+    // 수정 화면으로 이동
+    @GetMapping("/{board_id}/edit")
+    public String updateItemForm(@PathVariable("board_id") Long boardId, Model model) {
+        Board board = boardService.findOne(boardId);
+        model.addAttribute("board", board);
+
+        return "board/v2/editBoard";
     }
 }
