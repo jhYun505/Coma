9375b82aad42de61e008d9fc3a84a3e6e9f11447
[CLOUD4] 윤지현 <jihyeon.yun619@gmail.com>
2024-08-23 10:37:27 +0900

    feat: 게시글 추가버튼 로그인 여부에 따라 렌더링 결정

--- src/main/java/com/coma/coma/post/controller/PostController.java
+++ src/main/java/com/coma/coma/post/controller/PostController.java
@@ -7,6 +7,14 @@ import com.coma.coma.comments.Service.CommentService;
 import com.coma.coma.post.dto.Page;
 import com.coma.coma.post.dto.PostResponseDto;
 import com.coma.coma.post.service.PostService;
+import com.coma.coma.security.CustomUserDetails;
+import com.coma.coma.users.dto.UserDto;
+import com.coma.coma.users.service.UserService;
+import jakarta.annotation.Nullable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
@@ -21,11 +29,13 @@ public class PostController {
     private final PostService postService;
     private final BoardService boardService;
     private final CommentService commentService;
+    private final UserService userService;
 
-    public PostController(PostService postService, BoardService boardService, CommentService commentService) {
+    public PostController(PostService postService, BoardService boardService, CommentService commentService, UserService userService) {
         this.postService = postService;
         this.boardService = boardService;
         this.commentService = commentService;
+        this.userService = userService;
     }
 
     // 게시물 목록 페이지 - 페이지네이션 적용
@@ -34,8 +44,10 @@ public class PostController {
                            @RequestParam(defaultValue = "1") int page,
                            @RequestParam(defaultValue = "10") int size,
                            @RequestParam(value = "keyword", required = false) String keyword,
+                           @Nullable @AuthenticationPrincipal CustomUserDetails customUserDetails,      // 로그인된 user 정보 가져오기
                            Model model) {
         Page<PostResponseDto> posts;
+        boolean isAuthenticated = customUserDetails != null;
 
         if (keyword != null && !keyword.isEmpty()) {
             posts = postService.findByKeywordWithPagination(boardId, keyword, page, size);
@@ -46,6 +58,7 @@ public class PostController {
         model.addAttribute("posts", posts);
         model.addAttribute("board", board);
         model.addAttribute("keyword", keyword);
+        model.addAttribute("isAuthenticated", isAuthenticated);
         return "board/board";
     }
 
@@ -54,6 +67,7 @@ public class PostController {
     public String getPost(@PathVariable("postId") Integer postId, Model model) {
         PostResponseDto postResponseDto = postService.findPost(postId);
         List<Comment> comments = commentService.getCommentsByPostId(postId);
+
         model.addAttribute("post", postResponseDto);
         model.addAttribute("comments", comments);
         return "post/post";
