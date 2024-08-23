3882da9186b3a34c7a56f3ad9ebdfb4b6c334b15
[CLOUD4] 유호성 <hhss5435@gmail.com>
2024-08-23 20:54:09 +0900

    feature: add userId, groupId 유저아이디와 그룹아이디 포함하여 댓글 생성하도록 기능 추가 및 수정 시 수정할 댓글 데이터 반환 부분 삭제(front에서 이미 처리되어 있음)

--- src/main/java/com/coma/coma/comments/Controller/CommentController.java
+++ src/main/java/com/coma/coma/comments/Controller/CommentController.java
@@ -2,8 +2,14 @@ package com.coma.coma.comments.Controller;
 
 import com.coma.coma.comments.Dto.CommentDto;
 import com.coma.coma.comments.Entity.Comment;
+import com.coma.coma.comments.Mapper.CommentMapper;
 import com.coma.coma.comments.Service.CommentService;
 import com.coma.coma.post.service.PostService;
+import com.coma.coma.security.CustomUserDetails;
+import com.coma.coma.users.dto.UserResponseDto;
+import com.coma.coma.users.service.UserService;
+import jakarta.annotation.Nullable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,34 +21,39 @@ import java.util.List;
 public class CommentController {
 
     private final CommentService commentService;
+    private final UserService userService;
+    private final CommentMapper commentMapper;
 
-    public CommentController(CommentService commentService) {
+    public CommentController(CommentService commentService, UserService userService, CommentMapper commentMapper) {
         this.commentService = commentService;
+        this.userService = userService;
+        this.commentMapper = commentMapper;
     }
 
     @GetMapping("/{postId}")
-    public String getComments(@PathVariable int postId, Model model) {
+    public String getComments(@PathVariable int postId, Model model, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         List<Comment> comments = commentService.getCommentsByPostId(postId);
         model.addAttribute("comments", comments);
         model.addAttribute("postId", postId);
+        model.addAttribute("user", customUserDetails);
         return "post/post";
     }
 
     @PostMapping
-    public String addComment(@ModelAttribute CommentDto comment) {
+    public String addComment(@ModelAttribute CommentDto commentDto, @Nullable @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        if(customUserDetails == null){
+            return "redirect:/users/login";
+        }
+        Comment comment = commentMapper.toEntity(commentDto);
+        comment.setUserId(customUserDetails.getUserId());
+        comment.setGroupId(customUserDetails.getGroupId());
         commentService.addComment(comment);
         return "redirect:/page/posts/" + comment.getPostId();
     }
 
-    @GetMapping("/{commentId}/edit")
-    public String editCommentForm(@PathVariable int commentId, Model model) {
-        CommentDto comment = commentService.getCommentById(commentId);
-        model.addAttribute("comment", comment);
-        return "post/post";
-    }
-
     @PostMapping("/{commentId}/update")
-    public String editComment(@ModelAttribute CommentDto comment) {
+    public String editComment(@ModelAttribute CommentDto commentDto) {
+        Comment comment = commentMapper.toEntity(commentDto);
         commentService.updateComment(comment);
         int postId = commentService.getPostIdByCommentId(comment.getCommentId());
         return "redirect:/page/posts/" + postId;
