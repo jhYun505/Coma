98866014b22c24b725e420d6af443fa06cfa20fd
[CLOUD4] 윤지현 <jihyeon.yun619@gmail.com>
2024-08-23 14:49:16 +0900

    fix: 안쓰는 라이브러리 삭제

--- src/main/java/com/coma/coma/post/controller/PostRestController.java
+++ src/main/java/com/coma/coma/post/controller/PostRestController.java
@@ -14,7 +14,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.AccessDeniedException;
 
 @RestController
 @RequestMapping("/api/posts")
