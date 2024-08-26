4d0ac8672c6d6d4f2c779555e6bd83509c91525f
[CLOUD4] 이서율 <yul9910@naver.com>
2024-08-26 10:25:24 +0900

    fix : 유저 임시 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,6 +9,9 @@ VALUES (2, 'Users', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO Users (group_id, id, password, name, phone_number, is_delete, modified_date, signup_date)
 VALUES (1, 'admin', '$2a$10$s4Tj0wgK19PZY07r989kLewVCvdjtA6.UTIVZE7iJtn/5PRJ0m4ai', 'Admin Users', '01012345678', 'N', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
+INSERT INTO Users (group_id, id, password, name, phone_number, is_delete, modified_date, signup_date)
+VALUES (2, 'user1', '$2a$10$2WRUXjkW.Zwpp4p2BNkrpeQUqkroGsGfbFeX0amuEtxHnm8LnQMxK', '유저1', '01098765432', 'N', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
 -- board 테이블 초기 데이터 삽입
 INSERT INTO board
 (board_title, board_description, created_date, user_id)
